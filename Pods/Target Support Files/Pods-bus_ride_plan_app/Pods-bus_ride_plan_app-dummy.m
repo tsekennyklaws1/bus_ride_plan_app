72a0d80303656ccf988e2e9dae8860c9fad1d9c2
Kenny <tsekennykl1@gmail.com>
2024-02-20 22:41:53 +0800

    bug fixed for search filter on BusStop list

--- Pods/Target Support Files/Pods-bus_ride_plan_app/Pods-bus_ride_plan_app-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_bus_ride_plan_app : NSObject
-@end
-@implementation PodsDummy_Pods_bus_ride_plan_app
-@end
--- README.md
+++
@@ -1,2 +0,0 @@
-# bus_ride_plan_app
-Bus Estimated Time Arrival data retrieval app for Hong Kong Bus
